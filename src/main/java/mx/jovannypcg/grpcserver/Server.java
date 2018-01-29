aa6131a6e5eae9411be5cd0f6657b53e662cbf22
Jovanny Cruz <jovannypcg@yahoo.com>
2018-01-28 18:31:48 -0600

    Add implementation for Greeter

--- src/main/java/mx/jovannypcg/grpcserver/Server.java
+++ src/main/java/mx/jovannypcg/grpcserver/Server.java
@@ -1,6 +1,7 @@
 package mx.jovannypcg.grpcserver;
 
 import io.grpc.ServerBuilder;
+import mx.jovannypcg.grpcserver.services.impl.GreeterImpl;
 
 import java.io.IOException;
 import java.util.logging.Logger;
@@ -12,12 +13,44 @@ public class Server {
 
     private io.grpc.Server server;
 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final Server server = new Server();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
     private void start() throws IOException {
         server = ServerBuilder
                 .forPort(PORT)
+                .addService(new GreeterImpl())
                 .build()
                 .start();
 
         LOGGER.info("gRPC server started. Listening on " + PORT);
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                Server.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
     }
 }
--- src/main/java/mx/jovannypcg/grpcserver/services/impl/GreeterImpl.java
+++ src/main/java/mx/jovannypcg/grpcserver/services/impl/GreeterImpl.java
@@ -1,5 +1,19 @@
 package mx.jovannypcg.grpcserver.services.impl;
 
-public class GreeterImpl {
+import io.grpc.stub.StreamObserver;
+import mx.jovannypcg.grpcserver.messages.HelloReply;
+import mx.jovannypcg.grpcserver.messages.HelloRequest;
+import mx.jovannypcg.grpcserver.services.GreeterGrpc;
 
+public class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply
+                .newBuilder()
+                .setMessage("Hello " + request.getName())
+                .build();
+
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
 }
